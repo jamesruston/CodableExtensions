5fd781f30bc68aa2f8c5369f829b3b48ff227851
James Ruston <jruston90@gmail.com>
2019-06-24 11:50:19 +0100

    updated to Swift 5

--- Classes/CodableExtensions.swift
+++ Classes/CodableExtensions.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 public extension KeyedDecodingContainer {
-    public func decode<Transformer: DecodingContainerTransformer>(_ key: KeyedDecodingContainer.Key,
+    func decode<Transformer: DecodingContainerTransformer>(_ key: KeyedDecodingContainer.Key,
                                                            transformer: Transformer) throws -> Transformer.Output where Transformer.Input : Decodable {
         let decoded: Transformer.Input = try self.decode(key)
         
         return try transformer.transform(decoded)
     }
     
-    public func decode<T>(_ key: KeyedDecodingContainer.Key) throws -> T where T : Decodable {
+    func decode<T>(_ key: KeyedDecodingContainer.Key) throws -> T where T : Decodable {
         return try self.decode(T.self, forKey: key)
     }
 }
 
 public extension KeyedEncodingContainer {
     
-    public mutating func encode<Transformer: EncodingContainerTransformer>(_ value: Transformer.Output,
-                                                                  forKey key: KeyedEncodingContainer.Key,
-                                                                  transformer: Transformer) throws where Transformer.Input : Encodable {
+    mutating func encode<Transformer: EncodingContainerTransformer>(_ value: Transformer.Output,
+                                                                    forKey key: KeyedEncodingContainer.Key,
+                                                                    transformer: Transformer) throws where Transformer.Input : Encodable {
         let transformed: Transformer.Input = try transformer.transform(value)
         try self.encode(transformed, forKey: key)
     }
